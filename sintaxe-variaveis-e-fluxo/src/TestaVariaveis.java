b560f2b71218e6410e5b5ffaf1e433e87da95085
letyresina <leticiaresina@gmail.com>
2023-02-27 18:44:36 -0300

    Subindo ajustes no código

--- sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -2,10 +2,18 @@
 public class TestaVariaveis {
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Seja bem vindo ao programa de testes de Idade!");
 		int idade;
 		idade = 18;
 		
-		System.out.println(idade);
+		System.out.println("A idade é " + idade);
+		
+		/*Alguns testes com operadores aritmético*/
+		
+		int novaIdade = idade + 10;
+		
+		System.out.println("A nova idade é " + novaIdade + " Obrigada!");
 	}
 	
 }
